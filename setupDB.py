e9d9d7c8063b42b126eae95c2e639a9ac7a36f5b
Radman4 <nootnoot432@gmail.com>
2025-04-11 19:40:33 -0500

    Added remianing Law questions. it now has 10 questions as well. 2 categories of questions remain, will be done tomorrow.

--- setupDB.py
+++ setupDB.py
@@ -24,6 +24,46 @@ cursor.execute('''INSERT INTO QuizBowlDB
                         ("Law","What is statue Congress enacts to create a administrative agency?",
                          "1. Debuting Legislation", "2. Enabling Legislation", "3. Creating Legislation", "4. Enforcing Legislation", "2. Enabling Legislation"))
 
+cursor.execute('''INSERT INTO QuizBowlDB
+                       (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
+                        ("Law","True or False: The Federal Trade Commission is NOT an example of a independent agency.",
+                         "1. True", "2. False", "3. Null", "4. Null", "2. False"))
+
+cursor.execute('''INSERT INTO QuizBowlDB
+                       (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
+                        ("Law","What is the process by which courts decide on the constitutionality of legislative enactments and actions of the executive branch",
+                         "1. First Review", "2. Final Review", "3. Peer Review", "4. Judicial Review", "4. Juidicial Review"))
+
+cursor.execute('''INSERT INTO QuizBowlDB
+                       (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
+                        ("Law","True or False: The Constitution is the supreme law of the land.",
+                         "1. True", "2. False", "3. Null", "4. Null", "1. True"))
+
+cursor.execute('''INSERT INTO QuizBowlDB
+                          (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
+                            ("Law","What is NOT a type of Attoruney Fee?.",
+                             "1. Fixed Fees", "2. Winners Fees", "3. Hourly Fees", "4. Contigency Fees", "2. Winners Fees"))
+
+cursor.execute('''INSERT INTO QuizBowlDB
+                       (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
+                        ("Law","River City enacts an ordinance that prohibits all advertising on the sides of trucks. A court would likely review this ordinance under the principles of",
+                         "1. equal protection.", "2. free exercise.", "3. interstate commerce.", "4. due process", "4. due process"))
+
+cursor.execute('''INSERT INTO QuizBowlDB
+                       (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
+                        ("Law","A statute enacted by the Wyoming state legislature to regulate trucking affects interstate commerce. In evaluating this statute, the courts will weigh the burden that it imposes on interstate commerce against",
+                         "1. the federal government's authority to regulate the matter.", "2. the purpose of interstate commerce.", "3. the state's interest in regulating the matter.", "4. 	the statute's impact on noneconomic activity.", "3. the state's interest in regulating the matter."))
+
+cursor.execute('''INSERT INTO QuizBowlDB
+                       (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
+                        ("Law","True or False: New administrative agencies are rarely created in response to a crisis.",
+                         "1. True", "2. False", "3. Null", "4. Null", "2. False"))
+
+cursor.execute('''INSERT INTO QuizBowlDB
+                       (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
+                        ("Law","Holly files an employment discrimination suit against Industrial Inc. under Title VII of the Civil Rights Act on a disparate-impact theory. To succeed, Holly must show in part that she was adversely affected by the employer's",
+                         "1. Practice", "2. any of the choices", "3. business necessity", "4. 	bona fide occupational qualification.", "1. Practice"))
+
 # DS 3850 Questions
 cursor.execute('''INSERT INTO QuizBowlDB
                        (Category, prompt, OptionA, OptionB, OptionC, OptionD, Answer) VALUES (?,?,?,?,?,?,?);''' ,
